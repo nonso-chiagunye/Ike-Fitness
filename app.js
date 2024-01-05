1ebe5d2ca9be7a35fcde548f2d3f991476b90ab5
nonso-chiagunye <nonsoasoby@hotmail.com>
2024-01-05 17:40:30 +0400

    Implemented Cors

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const { xss } = require('express-xss-sanitizer');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -23,7 +24,9 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // 1) GLOBAL MIDDLEWARES
+app.use(cors());
 
+app.options('*', cors());
 // app.use(express.static(`${__dirname}/public`));
 app.use(express.static(path.join(__dirname, 'public')));
 
